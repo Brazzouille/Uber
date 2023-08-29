b250e20191a0d0930cbea519659ef128ff2e0793
abeaudoin <aurelien9198@gmail.com>
2023-08-29 13:11:52 +0200

    First BDD with the POST

--- src/main/java/com/imt/framework/com/Uber/entities/Plat.java
+++ src/main/java/com/imt/framework/com/Uber/entities/Plat.java
@@ -26,5 +26,51 @@ public class Plat {
     @Column(name = "prix")
     private Double prix;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCalories() {
+        return calories;
+    }
+
+    public void setCalories(String calories) {
+        this.calories = calories;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getChef() {
+        return chef;
+    }
+
+    public void setChef(String chef) {
+        this.chef = chef;
+    }
+
+    public Double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(Double prix) {
+        this.prix = prix;
+    }
 }
